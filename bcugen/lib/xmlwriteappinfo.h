cf25f56dc233961e8c70aafdeb6b6b7ee0ddc52a
muser <muser>
2005-03-15 14:48:35 +0000

    xmlio

---
+++ bcugen/lib/xmlwriteappinfo.h
@@ -0,0 +1,27 @@
+/*
+    BCU SDK bcu development enviroment
+    Copyright (C) 2005 Martin Kögler <mkoegler@auto.tuwien.ac.at>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef XMLWRITE_APP_INFO_H
+#define XMLWRITE_APP_INFO_H
+
+#include "classes.h"
+
+void WriteAppInfo (const Device * d, const char *file);
+
+#endif
