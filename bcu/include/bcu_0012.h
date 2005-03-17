8b9afa41db3ae1c97d6296604a769c193b1699ee
muser <muser>
2005-03-17 08:51:35 +0000

    BCU libs

---
+++ bcu/include/bcu_0012.h
@@ -0,0 +1,40 @@
+/*
+    BCU SDK bcu development enviroment
+    Copyright (C) 2005 Martin Kögler <mkoegler@auto.tuwien.ac.at>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    In addition to the permissions in the GNU General Public License, 
+    you may link the compiled version of this file into combinations
+    with other programs, and distribute those combinations without any 
+    restriction coming from the use of this file. (The General Public 
+    License restrictions do apply in other respects; for example, they 
+    cover modification of the file, and distribution when not linked into 
+    a combine executable.)
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef _BCU_0012_H
+#define _BCU_0012_H
+
+#include "bcu_common.h"
+
+static void inline transmit_groupobject (uchar no) {
+  if(!__builtin_constant_p(no)) 
+asm volatile ("lda %0\n\tjsr U_transRequest" : : "r"(no) : "A","X","RegB","RegC","RegJ");
+else
+asm volatile ("lda $%0\n\tjsr U_transRequest" : : "i"(no) : "A","X","RegB","RegC","RegJ");
+ }
+
+#endif
