15fca570a72db99100d1e786b7f2d14e3c93340c
muser <muser>
2005-04-26 11:13:59 +0000

    Property Datentypen

---
+++ bcu/include/proptypes.h
@@ -0,0 +1,83 @@
+/*
+    BCU SDK bcu development enviroment
+    Copyright (C) 2005 Martin Kögler <mkoegler@auto.tuwien.ac.at>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    In addition to the permissions in the GNU General Public License, 
+    you may link the compiled version of this file into combinations
+    with other programs, and distribute those combinations without any 
+    restriction coming from the use of this file. (The General Public 
+    License restrictions do apply in other respects; for example, they 
+    cover modification of the file, and distribution when not linked into 
+    a combine executable.)
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef PROP_TYPES_H
+#define PROP_TYPES_H
+
+typedef sint1 PROP1_T;
+typedef uint1 PROP2_T;
+typedef sint2 PROP3_T;
+typedef uint2 PROP4_T;
+typedef uint2 PROP5_T;
+typedef uint3 PROP6_T;
+typedef uint3 PROP7_T;
+typedef sint4 PROP8_T;
+typedef uint4 PROP9_T;
+typedef float PROP10_T;
+typedef double PROP11_T;
+typedef uint1 PROP12_T[10];
+typedef uint3 PROP13_T;
+typedef uint5 PROP14_T;
+
+typedef uint1 PROP17_T;
+typedef uint2 PROP18_T;
+typedef uint3 PROP19_T;
+typedef uint4 PROP20_T;
+typedef uint5 PROP21_T;
+typedef uint6 PROP22_T;
+typedef uint7 PROP23_T;
+typedef uint8 PROP24_T;
+typedef uint1 PROP25_T[9];
+typedef uint1 PROP26_T[10];
+
+typedef PROP1_T EPROP1_T EEPROM_ATTRIB;
+typedef PROP2_T EPROP2_T EEPROM_ATTRIB;
+typedef PROP3_T EPROP3_T EEPROM_ATTRIB;
+typedef PROP4_T EPROP4_T EEPROM_ATTRIB;
+typedef PROP5_T EPROP5_T EEPROM_ATTRIB;
+typedef PROP6_T EPROP6_T EEPROM_ATTRIB;
+typedef PROP7_T EPROP7_T EEPROM_ATTRIB;
+typedef PROP8_T EPROP8_T EEPROM_ATTRIB;
+typedef PROP9_T EPROP9_T EEPROM_ATTRIB;
+typedef PROP10_T EPROP10_T EEPROM_ATTRIB;
+typedef PROP11_T EPROP11_T EEPROM_ATTRIB;
+typedef PROP12_T EPROP12_T EEPROM_ATTRIB;
+typedef PROP13_T EPROP13_T EEPROM_ATTRIB;
+typedef PROP14_T EPROP14_T EEPROM_ATTRIB;
+
+typedef PROP17_T EPROP17_T EEPROM_ATTRIB;
+typedef PROP18_T EPROP18_T EEPROM_ATTRIB;
+typedef PROP19_T EPROP19_T EEPROM_ATTRIB;
+typedef PROP20_T EPROP20_T EEPROM_ATTRIB;
+typedef PROP21_T EPROP21_T EEPROM_ATTRIB;
+typedef PROP22_T EPROP22_T EEPROM_ATTRIB;
+typedef PROP23_T EPROP23_T EEPROM_ATTRIB;
+typedef PROP24_T EPROP24_T EEPROM_ATTRIB;
+typedef PROP25_T EPROP25_T EEPROM_ATTRIB;
+typedef PROP26_T EPROP26_T EEPROM_ATTRIB;
+
+#endif
