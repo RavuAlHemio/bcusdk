61385817afbd3225c9dcaa61b9a7f6f275a719e7
Martin Koegler <mkoegler@auto.tuwien.ac.at>
2009-11-05 15:17:55 +0100

    Correct FSF address
    Signed-off-by: Martin Koegler <mkoegler@auto.tuwien.ac.at>

--- bcu/lib/bcu2func.s
+++ bcu/lib/bcu2func.s
@@ -21,7 +21,7 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program; if not, write to the Free Software
-#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 	.global OR_TAB, AND_TAB, U_flagsGet, U_flagsSet, U_testObj, U_transRequest
 	.global EEwrite, EEsetChecksum, U_debounce, U_deb10, U_deb30, U_delMsgs
